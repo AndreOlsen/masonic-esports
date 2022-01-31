0ee98a6a3e26968010bd376989f1ef5792cbb0e8
Andre Olsen <andre_w.o@outlook.com>
2022-01-31 01:15:26 +0100

    Added new font versions. Added font preloading.

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 <?php 
+/**
+ * Masonic functions and definitions
+ *
+ * @package Masonic
+ * @since 1.0.0
+ */
 
-    /**
-     * Add WordPress functions support to theme.
-     */
-    if(!function_exists('masonic_theme_setup')) {
+
+    if(!function_exists('masonic_theme_setup')) :
+        /**
+         * Add WordPress functions support to theme.
+         */
         function masonic_theme_setup() {
             // Let WordPress manage site title.
             add_theme_support('title-tag');
@@ -48,30 +55,112 @@
 
             // Custom background color.
 		    add_theme_support('custom-background', array('default-color' => '#0E1626'));
+
+            // Block editor pre-defined color palette.
+            $masonic_blue         = '#0E1626';
+            $masonic_purple       = '#57009B';
+            $masonic_pink         = '#872783';
+            $masonic_lighter_blue = '#2E3186';
+            $ghost_white          = '#F8F8FF';
+            $black                = '#000000';
+
+            add_theme_support(
+                'editor-color-palette',
+                array(
+                    array(
+                        'name'  => __('Masonic Blue', 'masonic_esports'),
+                        'slug'  => 'masonic-blue',
+                        'color' => $masonic_blue
+                    ),
+                    array(
+                        'name'  => __('Masonic Lighter Blue', 'masonic_esports'),
+                        'slug'  => 'masonic-lighter-blue',
+                        'color' => $masonic_lighter_blue
+                    ),
+                    array(
+                        'name'  => __('Masonic Purple', 'masonic_esports'),
+                        'slug'  => 'masonic-purple',
+                        'color' => $masonic_purple
+                    ),
+                    array(
+                        'name'  => __('Masonic Pink', 'masonic_esports'),
+                        'slug'  => 'masonic-pink',
+                        'color' => $masonic_pink
+                    ),
+                    array(
+                        'name'  => __('Ghost White', 'masonic_esports'),
+                        'slug'  => 'ghost-white',
+                        'color' => $ghost_white
+                    ),
+                    array(
+                        'name'  => __('Black', 'masonic_esports'),
+                        'slug'  => 'black',
+                        'color' => $black
+                    )
+                )
+            );
+
+            add_theme_support(
+                'editor-gradient-presets',
+                array(
+                    array(
+                        'name'     => __('Pink to Blue', 'masonic_esports'),
+                        'slug'     => 'pink-to-blue',
+                        'gradient' => 'linear-gradient(200deg, ' . $masonic_pink . ' 0%, ' . $masonic_lighter_blue . ' 100%)'
+                    )
+                )
+            );
         }
-    }
+
+    endif;
 
     add_action('after_setup_theme', 'masonic_theme_setup');
 
-    /**
-     * Enqueue theme styles.
-     */
-    function masonic_enqueue_styles() {
-        wp_enqueue_style('masonic-style', get_stylesheet_uri(), array(), '1.0.0', 'all');
-    }
+    if(!function_exists('masonic_enqueue_styles')) :
+
+        /**
+         * Enqueue theme styles.
+         */
+        function masonic_enqueue_styles() {
+            wp_enqueue_style('masonic-style', get_stylesheet_uri(), array(), '1.0.0', 'all');
+        }
+
+    endif;
 
     add_action('wp_enqueue_scripts', 'masonic_enqueue_styles');
 
-    /**
-     * Enqueue theme scripts.
-     */
-    function masonic_enqueue_scripts() {
-        wp_enqueue_script('header-scroll', get_stylesheet_directory_uri() . '/assets/js/header-scroll.js', array(), '1.0.0');
-    }
+    if(!function_exists('masonic_enqueue_scripts')) :
+
+        /**
+         * Enqueue theme scripts.
+         */
+        function masonic_enqueue_scripts() {
+            wp_enqueue_script('header-scroll', get_stylesheet_directory_uri() . '/assets/js/header-scroll.js', array(), '1.0.0');
+        }
+
+    endif;
 
     add_action('wp_enqueue_scripts', 'masonic_enqueue_scripts');
 
-    include 'inc/masonic-patterns.php';
+    if(!function_exists('masonic_preload_webfonts')) :
+
+        /**
+         * Preloads the main web font to improve performance.
+         *
+         * @since 1.0.0
+         */
+        function masonic_preload_webfonts() {
+            ?>
+            <link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/D-DIN-PRO-400-Regular.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
+            <?php
+        }
+    
+    endif;
+
+    add_action('wp_head', 'masonic_preload_webfonts');
+
+    // Block patterns.
+    require 'inc/masonic-patterns.php';
 
     add_filter( 'oembed_response_data', 'disable_embeds_filter_oembed_response_data_' );
 
@@ -80,113 +169,123 @@
     add_post_type_support( 'players', 'thumbnail' );
     add_post_type_support( 'management', 'thumbnail' );
 
-    /**
-     * Register custom post types.
-     */
-    function cpt_init() {
+    if(!function_exists('cpt_init')) :
 
-        // Partners.
-        $args = array(
-            'labels' => array(
-                'name'          => _x('Partners', 'Post Type General Name', 'masonic-esports'),
-                'singular_name' => _x('Partner', 'Post Type Singular Name', 'masonic-esports'),
-                'all_items'     => __('All Partners', 'masonic-esports'),
-                'view_item'     => __('View Partner', 'masonic-esports'),
-                'add_new_item'  => __('Add New Partner', 'masonic-esports'),
-            ),
-            'public'              => true,
-            'publicly_queryable'  => true,
-            'menu_icon'           => 'dashicons-businessman',
-            'show_in_rest'        => true
-        );
+        /**
+         * Register custom post types.
+         */
+        function cpt_init() {
 
-        register_post_type('partners', $args);
-
-        // Players.
-        $args = array(
-            'labels' => array(
-                'name'          => _x('Players', 'Post Type General Name', 'masonic-esports'),
-                'singular_name' => _x('Player', 'Post Type Singular name', 'masonic-esports'),
-                'all_items'     => __('All Players', 'masonic-esports'),
-                'view_item'     => __('View Player', 'masonic-esports'),
-                'add_new_item'  => __('Add New Player', 'masonic-esports'),
-            ),
-            'public'              => true,
-            'publicly_queryable'  => true,
-            'menu_icon'           => 'dashicons-groups',
-            'show_in_rest'        => true
-        );
-
-        register_post_type('players', $args);
-
-        // Teams taxonomy for Players cpt.
-        register_taxonomy(
-            'teams',
-            'players',
-            array(
+            // Partners.
+            $args = array(
                 'labels' => array(
-                    'name'          => __('Teams', 'masonic-esports'),
-                    'singular_name' => __('Team','masonic-esports'),
-                    'add_new_item'  => 'New team',
-                    'new_item_name' => 'New team',
+                    'name'          => _x('Partners', 'Post Type General Name', 'masonic-esports'),
+                    'singular_name' => _x('Partner', 'Post Type Singular Name', 'masonic-esports'),
+                    'all_items'     => __('All Partners', 'masonic-esports'),
+                    'view_item'     => __('View Partner', 'masonic-esports'),
+                    'add_new_item'  => __('Add New Partner', 'masonic-esports'),
                 ),
-                'show_ui'       => true,
-                'show_tagcloud' => false,
-                'hierarchical'  => true
-            )
-        );
+                'public'              => true,
+                'publicly_queryable'  => true,
+                'menu_icon'           => 'dashicons-businessman',
+                'show_in_rest'        => true
+            );
 
-        // Management.
-        $args = array(
-            'labels' => array(
-                'name'          => _x('Management', 'Post Type General Name', 'masonic-esports'),
-                'singular_name' => _x('Management', 'Post Type Singular Name', 'masonic-esports'),
-                'all_items'     => __('All Managers', 'masonic-esports'),
-                'view_item'     => __('View Mangement', 'masonic-esports'),
-                'add_new_item'  => __('Add New Manager', 'masonic-esports'),
-            ),
-            'public'              => true,
-            'has_archive'         => true,
-            'menu_icon'           => 'dashicons-businessperson',
-            'publicly_queryable'  => false,
-        );
+            register_post_type('partners', $args);
 
-        register_post_type('management', $args);
-    }
+            // Players.
+            $args = array(
+                'labels' => array(
+                    'name'          => _x('Players', 'Post Type General Name', 'masonic-esports'),
+                    'singular_name' => _x('Player', 'Post Type Singular name', 'masonic-esports'),
+                    'all_items'     => __('All Players', 'masonic-esports'),
+                    'view_item'     => __('View Player', 'masonic-esports'),
+                    'add_new_item'  => __('Add New Player', 'masonic-esports'),
+                ),
+                'public'              => true,
+                'publicly_queryable'  => true,
+                'menu_icon'           => 'dashicons-groups',
+                'show_in_rest'        => true
+            );
+
+            register_post_type('players', $args);
+
+            // Teams taxonomy for Players cpt.
+            register_taxonomy(
+                'teams',
+                'players',
+                array(
+                    'labels' => array(
+                        'name'          => __('Teams', 'masonic-esports'),
+                        'singular_name' => __('Team','masonic-esports'),
+                        'add_new_item'  => 'New team',
+                        'new_item_name' => 'New team',
+                    ),
+                    'show_ui'       => true,
+                    'show_tagcloud' => false,
+                    'hierarchical'  => true
+                )
+            );
+
+            // Management.
+            $args = array(
+                'labels' => array(
+                    'name'          => _x('Management', 'Post Type General Name', 'masonic-esports'),
+                    'singular_name' => _x('Management', 'Post Type Singular Name', 'masonic-esports'),
+                    'all_items'     => __('All Managers', 'masonic-esports'),
+                    'view_item'     => __('View Mangement', 'masonic-esports'),
+                    'add_new_item'  => __('Add New Manager', 'masonic-esports'),
+                ),
+                'public'              => true,
+                'has_archive'         => true,
+                'menu_icon'           => 'dashicons-businessperson',
+                'publicly_queryable'  => false,
+            );
+
+            register_post_type('management', $args);
+        }
+
+    endif;
 
     add_action('init', 'cpt_init');
 
-    /**
-     * Register menu areas.
-     */
-    function register_menus() {
-        register_nav_menus(
-            array(
-                'main-menu-left'    => __('Main Menu Left'),
-                'main-menu-right'   => __('Main Menu Right'),
-                'footer-menu-left'  => __('Footer Menu Left'),
-                'footer-menu-right' => __('Footer Menu Right')
-            )
-        );
-    }
+    if(!function_exists('register_menus')) :
+
+        /**
+         * Register menu areas.
+         */
+        function register_menus() {
+            register_nav_menus(
+                array(
+                    'main-menu-left'    => __('Main Menu Left'),
+                    'main-menu-right'   => __('Main Menu Right'),
+                    'footer-menu-left'  => __('Footer Menu Left'),
+                    'footer-menu-right' => __('Footer Menu Right')
+                )
+            );
+        }
+
+    endif;
 
     add_action('init', 'register_menus');
 
-    /**
-     * Register widget areas.
-     */
-    function register_widget_area() {
-        register_sidebar(
-            array(
-                'id'            => 'socials_widget_area',
-                'name'          => esc_html__( 'Socials', 'masonic-esports' ),
-                'description'   => esc_html__( 'Area for social icons', 'masonic-esports' ),
-                'before_widget' => '',
-                'after_widget'  => ''
-            )
-        );
-    }
+    if(!function_exists('register_widget_area')) :
 
-    add_action( 'widgets_init', 'register_widget_area' )
-    
-?>
+        /**
+         * Register widget areas.
+         */
+        function register_widget_area() {
+            register_sidebar(
+                array(
+                    'id'            => 'socials_widget_area',
+                    'name'          => esc_html__( 'Socials', 'masonic-esports' ),
+                    'description'   => esc_html__( 'Area for social icons', 'masonic-esports' ),
+                    'before_widget' => '',
+                    'after_widget'  => ''
+                )
+            );
+        }
+
+    endif;
+
+    add_action( 'widgets_init', 'register_widget_area' );
