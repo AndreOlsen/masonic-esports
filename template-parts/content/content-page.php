276ebf442248f19c6a1e30759e19a880cc66874f
Andre Olsen <andre_w.o@outlook.com>
2022-02-03 05:59:38 +0100

    Added teams page.

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -33,39 +33,71 @@ if($post->post_name === 'partners') :
     endif;
     
 elseif($post->post_name === 'teams') :
-    /* $players = get_posts(
-        array(
-            'post_type'   => 'players',
-            'numberposts' => -1,
-            'post_status' => 'publish',
-            'fields'      => 'ids'
-        )
-    ); */
+
     $categories = get_categories( array(
         'orderby' => 'name',
         'order'   => 'ASC',
         'taxonomy' => 'teams',
-        'hide_empty' => false,
     ) );
 
-    echo '<pre>';
-
     if(!empty($categories)) :
-        echo '<section>';
+        echo '<section class="teams">';
 
         foreach($categories as $category) :
-            var_dump($category);
-            //var_dump(get_the_category($player));
+            echo '<section class="teams__team">';
+            echo '<section class="team__headline">';
+            echo '<h2>' . $category->name . '</h2>';  
+            echo '</section>';
+
+            $players = get_posts(
+                array(
+                    'post_type'   => 'players',
+                    'numberposts' => -1,
+                    'post_status' => 'publish',
+                    'fields'      => 'ids',
+                    'tax_query'   => [
+                        [
+                            'taxonomy' => 'teams',
+                            'terms'    => $category->term_id,
+                        ],
+                    ],
+                )
+            );
+            
+            if(!empty($players)) :
+                echo '<section class="team__players">';
+
+                foreach($players as $player_id) :
+                    $image_id = get_field('player_image', $player_id);
 ?>
 
 
+                    <article class="player">
+                        <h3 class="player__ign"><?php the_field('player_ign', $player_id); ?></h3>
+                        <!-- <?php echo wp_get_attachment_image($image_id, 'medium'); ?> -->
+                        <!-- <img src="" alt="<?php /* echo get_the_title($player_id); */ ?>"> -->
+                        <figure class="player__image" style="background-image: url('<?php the_field('player_image', $player_id); ?>')"></figure>
+                        <div class="player__meta">
+                            <h3 class="player__name"><?php echo get_the_title($player_id); ?></h3>
+                            <p class="player__role"><?php the_field('player_role', $player_id); ?></p>
+                            <p class="player__nationality"><?php the_field('player_nationality', $player_id); ?></p>
+                            <div class="player__social">
+                                <?php echo \get_social_icons(get_field('player_socials', $player_id)); ?>
+                            </div>
+                        </div>
+                    </article>
 
 
+<?php           endforeach; // End players.
 
-<?php  
-        endforeach;
+                echo '</section>';
+            endif;
 
-        echo '<section>';
+            echo '</section>';
+        endforeach; // End categories.
+
+        echo '</section>';
     endif;
+
 endif;
 ?>
