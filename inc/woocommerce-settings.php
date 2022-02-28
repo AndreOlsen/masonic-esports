29b408c5eae9db5e2934406efe75166e8232214e
Andre Olsen <andre_w.o@outlook.com>
2022-02-28 12:06:41 +0100

    Styled single product page. For now.

--- inc/woocommerce-settings.php
+++ inc/woocommerce-settings.php
@@ -1,10 +1,11 @@
 <?php 
-
-// Remove "Select options" button from (variable) products on the main WooCommerce shop page.
-add_filter('woocommerce_loop_add_to_cart_link', 'remove_select_options_btn', 10, 1);
-
+/**
+ * emove "Select options" button from (variable) products on the main WooCommerce shop page.
+ *
+ * @param Object $product
+ * @return void
+ */
 function remove_select_options_btn($product) {
-
 	global $product;
 
 	if (is_shop() && 'variable' === $product->get_type()) {
@@ -19,8 +20,29 @@ function remove_select_options_btn($product) {
 		);
 	}
 }
+add_filter('woocommerce_loop_add_to_cart_link', 'remove_select_options_btn', 10, 1);
 
 // Remove sorting list.
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 // Remove results count.
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+// Remove product meta.
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
+/**  
+* Remove product data tabs  
+*/ add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 ); 
+function woo_remove_product_tabs( $tabs ) 
+{     unset( $tabs['additional_information'] );   
+  return $tabs; }
+
+/**
+ * Adds product title to single product page.
+ *
+ * @return void
+ */
+function add_single_product_title() {
+	global $product;
+    echo '<h2 class="woocommerce-product-title">'.$product->get_title().'</h2>';
+}
+add_filter( 'woocommerce_single_product_summary', 'add_single_product_title', 5);
