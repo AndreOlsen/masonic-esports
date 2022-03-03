8c18bdde109f5fb3b278fcc37e13d06451afca79
Andre Olsen <andre_w.o@outlook.com>
2022-03-03 17:19:43 +0100

    Customized accounts page, register form and added delete account.

--- inc/woocommerce-settings.php
+++ inc/woocommerce-settings.php
@@ -29,12 +29,17 @@ remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 // Remove product meta.
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
-/**  
-* Remove product data tabs  
-*/ add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 ); 
-function woo_remove_product_tabs( $tabs ) 
-{     unset( $tabs['additional_information'] );   
-  return $tabs; }
+/**
+ * Remove product data tabs.
+ *
+ * @param  array $tabs
+ * @return array
+ */
+function woo_remove_product_tabs( $tabs ) {
+	unset( $tabs['additional_information'] );   
+	return $tabs; 
+}
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 ); 
 
 /**
  * Adds product title to single product page.
@@ -46,3 +51,91 @@ function add_single_product_title() {
     echo '<h2 class="woocommerce-product-title">'.$product->get_title().'</h2>';
 }
 add_filter( 'woocommerce_single_product_summary', 'add_single_product_title', 5);
+
+/**
+ * Removes menu items from my account page.
+ *
+ * @param  array $items
+ * @return array
+ */
+function custom_my_account_menu_items( $items ) {
+    unset($items['downloads']);
+    return $items;
+}
+add_filter( 'woocommerce_account_menu_items', 'custom_my_account_menu_items' );
+
+/**
+ * Display custom registration fields in registration form.
+ *
+ * @return void
+ */
+function add_fields_to_register_form() {
+    ?>
+  
+    <p class="form-row form-row-first">
+    <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
+    <input type="text" class="woocommerce-Input input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
+    </p>
+  
+    <p class="form-row form-row-last">
+    <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
+    <input type="text" class="woocommerce-Input input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
+    </p>
+  
+    <div class="clear"></div>
+  
+    <?php
+}
+
+add_action( 'woocommerce_register_form_start', 'add_fields_to_register_form' );
+
+/**
+ * Validate custom registration fields.
+ *
+ * @param [type] $errors
+ * @param [type] $username
+ * @param [type] $email
+ * @return void
+ */
+function validate_custom_register_fields( $errors, $username, $email ) {
+	if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
+		$errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
+		$errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
+    }
+    return $errors;
+}
+
+add_filter( 'woocommerce_registration_errors', 'validate_custom_register_fields', 10, 3 );
+
+/**
+ * Save the custom registration fields values.
+ *
+ * @param  int $customer_id
+ * @return void
+ */
+function save_custom_register_fields( $customer_id ) {
+	if ( isset( $_POST['billing_first_name'] ) ) {
+		update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
+        update_user_meta( $customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) ) {
+		update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
+        update_user_meta( $customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']) );
+    }
+}
+
+add_action( 'woocommerce_created_customer', 'save_custom_register_fields' );
+
+/**
+ * Remove required fields
+ *
+ * @param  array $required_fields
+ * @return void
+ */
+function wc_save_account_details_required_fields( $required_fields ){
+	unset( $required_fields['account_display_name'] );
+    return $required_fields;
+}
+add_filter('woocommerce_save_account_details_required_fields', 'wc_save_account_details_required_fields' );
